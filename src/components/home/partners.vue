78646261e974c7927d2a1eaddd852629f7966973
Phil <matundaphyl@gmail.com>
2019-12-11 17:38:55 +0300

    entire project

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -79,12 +79,12 @@
                                         class="white elevation-0"
                                     >
                                     <v-card flat tile class="d-flex pa-2 ma-1 elevation-1" style="border-radius:5px;border-width: 1px; border-style: solid;">
-                                        <a href="https://gdgjalandhar.com" target="_blank">
+                                        <a href="https://github/DSCKabarak" target="_blank">
                                         <v-tooltip bottom>
                                             <template v-slot:activator="{ on }">
                                         <v-img
-                                            :src="require('@/assets/img/partners/gdgjalandhar.png')"
-                                            :lazy-src="require('@/assets/img/partners/gdgjalandhar.png')"
+                                            :src="require('@/assets/img/partners/dsckabarak.jpeg')"
+                                            :lazy-src="require('@/assets/img/partners/dsckabarak.jpeg')"
                                             aspect-ratio="3.0"
                                             class="white"
                                             v-on="on"
@@ -100,7 +100,7 @@
                                             </v-layout>
                                         </v-img>
                                         </template>
-                                        <span class="google-font">GDG Jalandhar</span>
+                                        <span class="google-font">DSC Kabarak</span>
                                         </v-tooltip>
                                         </a>
                                     </v-card>
